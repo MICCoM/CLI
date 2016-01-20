bc09a49cae04784c35d2c4e6c918960e0a5a8488
Andreas Wilke <wilke@mcs.anl.gov>
2016-01-20 11:39:09 -0600

    main access handle

--- lib/MICCOM/miccom.py
+++ lib/MICCOM/miccom.py
@@ -12,6 +12,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import jsonpickle
 
+
+
+def md5(fname):
+    hash = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
 class handle:
 
     def __init__(self, api, dataDir, code):
